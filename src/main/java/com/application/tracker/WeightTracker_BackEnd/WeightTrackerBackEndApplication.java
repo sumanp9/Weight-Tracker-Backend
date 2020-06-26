bdda4c04fa488b045200154e9ba60d87755d0ce2
sumanp9 <11.sumanpradhan@gmail.com>
2020-06-26 13:04:31 -0500

    Added sql configuration

---
+++ src/main/java/com/application/tracker/WeightTracker_BackEnd/WeightTrackerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.application.tracker.WeightTracker_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrackerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightTrackerBackEndApplication.class, args);
+	}
+
+}
